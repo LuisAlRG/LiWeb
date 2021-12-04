8817d861ae3c8b6c7c024579fc3ce393b77d442c
LuisAlRG <largmx2011@hotmail.com>
2021-12-04 21:31:25 +0000

    Cambio 2 04 11 21

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,19 +16,19 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         // seeder con datos predeterminados
-        
+        /*
         $this->call(EmpleadoSeeder::class);
         $this->call(AutorSeeder::class);
         $this->call(EditorialSeeder::class);
         $this->call(GeneroSeeder::class);
         $this->call(LibroSeeder::class);
         $this->call(VentaSeeder::class);
-
+        */
 
         //seeder con un solo usuario para poder hacer cosas desde 0
-        /*
+        
         $this->call(EmpleadoZeroSeeder::class);
         $this->call(GeneroZeroSeeder::class);
-        */
+        
     }
 }
